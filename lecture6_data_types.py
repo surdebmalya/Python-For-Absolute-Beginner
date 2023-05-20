65f69a819f29a65c0273fcd356069b15bafc5f90
Debmalya Sur <surdebmalya2001@gmail.com>
2023-05-20 18:27:22 +0530

    lecture 6 code added

---
+++ lecture6_data_types.py
@@ -0,0 +1,30 @@
+## type()
+## definition: using type function we can know the type of an object
+
+name = "Debmalya"
+x = type(name)
+print(x)
+
+num = 9
+y = type(num)
+print(y)
+
+## complex number
+comp = 1 + 6j
+print(comp)
+
+## adding two complex number
+comp2 = 2+9j
+comp3 = comp + comp2
+print(comp3)
+print(type(comp3))
+
+## homework
+num1 = 1+2j
+num2 = 2+5j
+
+add = num1 + num2
+print(add)
+
+mul = num1 * num2
+print(mul)
