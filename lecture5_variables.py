0e680483e237b123c6f2286ed0617bcb3bed6f00
Debmalya Sur <surdebmalya2001@gmail.com>
2023-05-20 12:07:05 +0530

    lecture 5 code added

---
+++ lecture5_variables.py
@@ -0,0 +1,40 @@
+## variables: Variables are containers for storing the data values for future use
+
+# integer variable
+x = 9
+print(x)
+
+# float variable
+y = 3.8
+print(y)
+
+# "", '' -> string variable
+name = 'Debmalya'
+print(name)
+
+# boolean variable -> True and False
+flag = True
+print(flag)
+
+# Redefinition is possible in python
+flag = "India"
+print(flag)
+
+X = 10
+print(x)
+print(X)
+
+# Homework solution:
+print("Solution of the homework")
+
+value = 18
+print(value)
+
+value = 16.7
+print(value)
+
+value = "Debmalya"
+print(value)
+
+value = False
+print(value)
