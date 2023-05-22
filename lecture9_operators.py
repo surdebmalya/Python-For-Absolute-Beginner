c2bbb44896e4b9c03ed732805533c38a54999c12
Debmalya Sur <surdebmalya2001@gmail.com>
2023-05-22 16:49:11 +0530

    operator code added

---
+++ lecture9_operators.py
@@ -0,0 +1,19 @@
+# 1. Arithmetic Operator
+# + - * / % ** //
+# Example:
+# x^y
+x = 3
+y = 2
+print(x ** y) # 3^2 = 9
+
+# 2. asignment operator
+# = += -= *= /=
+x = 5
+x += 1 # x = x+1
+print(x)
+
+# 3. Comparison Operator
+# ==, !=, >, <, >=, <=
+
+# 4. Logical Operator
+# and, or, not
